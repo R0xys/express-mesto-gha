8e11434b266c08dd703900477ed354a15d5e6c38
r0_xyz <maxhard243@gmail.com>
2023-07-09 15:31:12 +0300

    refactor: change method of error

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use("/users", require("./routes/users"));
 // eslint-disable-next-line import/newline-after-import
 app.use("/cards", require("./routes/cards"));
-app.get("*", (req, res) => {
+app.use("*", (req, res) => {
   res.status(404).send({ message: "Not found" });
 });
 
